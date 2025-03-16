fb7762c36c7a3fe2fb6a5aa2c72b9c33e4f4f57b
AndresPedroza-Games <andresepedrozaruni@gmail.com>
2025-03-16 14:51:13 +0100

    Added simple BombExplosion particle system (material left)

--- Assets/Scripts/PlayerController/AttackWeapons/Bomb/Bomb.cs
+++ Assets/Scripts/PlayerController/AttackWeapons/Bomb/Bomb.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
 
+    [SerializeField] private ParticleSystem bombExplosion;
+
+    private ParticleSystem currentBombExplosion;
+
     //Destruction of Bomb after Collision
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -12,7 +16,15 @@ public class Bomb : MonoBehaviour
         //Animation left!
         if (collision.gameObject.tag == "wall" || collision.gameObject.tag == "Enemy")
         {
+            Bombexplosion();
             Destroy(collision.gameObject);
         }        
+    }
+
+
+
+    private void Bombexplosion()
+    {
+        currentBombExplosion = Instantiate(bombExplosion,transform.position, Quaternion.identity);
     }
 }
