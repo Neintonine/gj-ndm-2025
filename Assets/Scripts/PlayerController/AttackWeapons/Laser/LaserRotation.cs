dbfc6f0b0d75cd198f41c000c16d372606c7315b
AndresPedroza-Games <andresepedrozaruni@gmail.com>
2025-03-16 14:13:17 +0100

    Added new LaserRotation system

--- Assets/Scripts/PlayerController/AttackWeapons/Laser/LaserRotation.cs
+++ Assets/Scripts/PlayerController/AttackWeapons/Laser/LaserRotation.cs
@@ -7,16 +7,33 @@ public class LaserRotation : MonoBehaviour
 
     [SerializeField] private float initalAngle;
 
+    private Transform laserTransform;
+    private CapsuleCollider2D ubootCollider;
+
+    private void Awake()
+    {
+        laserTransform = gameObject.transform.GetChild(0).GetComponent<Transform>();
+
+        ubootCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<CapsuleCollider2D>();
+    }
+
     public void Update()
     {
 
         Vector3 targetRotation = camera.ScreenToWorldPoint(Input.mousePosition);
 
-        float angleR = Mathf.Atan2(targetRotation.y - transform.position.y, targetRotation.x - transform.position.x);
+        Vector2 closestPoint = ubootCollider.ClosestPoint(targetRotation);
 
-        float angleD = (180 / Mathf.PI) * angleR - initalAngle;
+        Vector2 direction = (targetRotation - (Vector3)closestPoint).normalized;
+
+
+        float angleR = Mathf.Atan2(direction.y, direction.x);
+        float angleD = Mathf.Rad2Deg * angleR - initalAngle;
 
         transform.rotation = Quaternion.Euler(0,0,angleD);
+
+        laserTransform.position = closestPoint;
+
     }
 
 }
